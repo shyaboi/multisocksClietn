623c6bdb2589963a5920b0b0fe8cddfe4a1126ab
Shyaboi <ian.shane.sears@gmail.com>
2020-10-25 23:51:15 -0700

    added input and listener/emiter to tell server who is typing

--- src/components/ChatClient.tsx
+++ src/components/ChatClient.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { IonApp, IonHeader,  IonToolbar,  IonTitle, IonContent, IonList, IonItem, IonLabel } from '@ionic/react';
+import { IonApp, IonHeader,  IonToolbar,  IonTitle, IonContent, IonList, IonItem, IonLabel, IonInput } from '@ionic/react';
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://localhost:3000";
 const socket = socketIOClient(ENDPOINT);
@@ -7,6 +7,8 @@ const socket = socketIOClient(ENDPOINT);
 function ChatClient() {
   const [numberInRoom, setNumber] = useState("");
   const [inRoom, setInRoom] = useState("");
+  const [input, setInput] = useState("");
+  const [ name, setName ] = useState('');
 
   useEffect(() => {
     socket.on("welcome", (data: any) => {
@@ -17,29 +19,44 @@ function ChatClient() {
       setInRoom(data);
       // console.log(data)
     });
-    
+    socket.on("hello", (data: any) => {
+      setNumber(data);
+      // console.log(data)
+    });
+    socket.on("bye", (data: any) => {
+      setNumber(data);
+      console.log(data)
+    });
   }, []);
+
+  const thing = ()=> {
+    const id = socket.id
+    socket.emit('typing', {id})
+    console.log(socket.id+'is typing')
+  }
+
   const usersInRoom =Object.values(inRoom).map((user) => (
 <IonItem>
-        <IonLabel>{user}</IonLabel>
+        <IonLabel>User ID: {user}</IonLabel>
       </IonItem>
   ))
   return (
     <IonApp>
     <IonHeader>
       <IonToolbar>
-      <IonTitle> {numberInRoom}  {/* {inRoom} */}</IonTitle>
+      <IonTitle> {inRoom.length}  {/* {inRoom} */}</IonTitle>
       </IonToolbar>
     </IonHeader>
     <IonContent>
-Comense to chatting
-    </IonContent>
-    <IonContent>
-    {numberInRoom}
+    There are currently {inRoom.length} in the chat.
       <IonList>
        {usersInRoom}
     </IonList>
     </IonContent>
+    <IonItem>
+            <IonLabel position="stacked">Enter chat message</IonLabel>
+            <IonInput value={input} placeholder="Shout out your people!" onInput={thing} onIonChange={(e) => setInput((e.target as HTMLTextAreaElement).value)} />
+          </IonItem>
   </IonApp>
   );
 }
