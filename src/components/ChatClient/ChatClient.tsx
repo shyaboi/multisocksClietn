fdf030162cb740e35a8a3c054ac2a45997280d9b
Shyaboi <ian.shane.sears@gmail.com>
2020-11-05 14:56:45 -0700

    took out voice server for now for boot speed

--- src/components/ChatClient/ChatClient.tsx
+++ src/components/ChatClient/ChatClient.tsx
@@ -1,4 +1,4 @@
-import Voice from '../Voice/Voice'
+// import Voice from "../Voice/Voice";
 import React, { useState, useEffect, useRef } from "react";
 import {
   IonApp,
@@ -11,58 +11,64 @@ import {
   IonLabel,
   IonInput,
   IonToast,
-  IonFab, 
-  IonFabButton, 
+  IonFab,
+  IonFabButton,
   IonIcon,
-  IonModal, 
+  IonModal,
   IonButton,
   IonAvatar,
 } from "@ionic/react";
-import {  arrowBackCircle, peopleCircleOutline, imagesOutline, megaphoneOutline} from 'ionicons/icons';
+import {
+  arrowBackCircle,
+  peopleCircleOutline,
+  imagesOutline,
+  megaphoneOutline,
+} from "ionicons/icons";
 import socketIOClient from "socket.io-client";
-const shyaboiMP3 = require ('./shyaboi.mp3')
-const doorOpenMP3 = require ('./open_door_1.mp3')
-const closeDoorMP3 = require ('./close_door_1.mp3')
+const shyaboiMP3 = require("./shyaboi.mp3");
+const doorOpenMP3 = require("./open_door_1.mp3");
+const closeDoorMP3 = require("./close_door_1.mp3");
 const ENDPOINT = "https://dinguschatserver.herokuapp.com/";
 const socket = socketIOClient(ENDPOINT);
+
+
 function ChatClient() {
-  
   const [numberInRoom, setNumber] = useState("");
   const [inRoom, setInRoom] = useState("");
   const [input, setInput] = useState("");
   const [isTyping, setIsTyping] = useState("");
   const [leftChat, setLeftChat] = useState("");
   const [enterChat, setEnterChat] = useState("");
-  const [chatArr, setChatArr] = useState('');
+  const [chatArr, setChatArr] = useState("");
   const [showToast1, setShowToast1] = useState(false);
   const [showToast2, setShowToast2] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  
-  const bottomRef:any = useRef();
-  
+
+  const bottomRef: any = useRef();
+
   const scrollToBottom = () => {
     bottomRef.current.scrollIntoView({
       behavior: "smooth",
       block: "start",
     });
   };
-  
-  const shyaboi = new Audio(shyaboiMP3)
-  const doorOpen = new Audio(doorOpenMP3)
-  const doorClose = new Audio(closeDoorMP3)
-  
-  useEffect(() => {
-    scrollToBottom()
 
-    const openMediaDevices = async (constraints:any) => {
+  const shyaboi = new Audio(shyaboiMP3);
+  const doorOpen = new Audio(doorOpenMP3);
+  const doorClose = new Audio(closeDoorMP3);
+
+  useEffect(() => {
+    scrollToBottom();
+
+    const openMediaDevices = async (constraints: any) => {
       return await navigator.mediaDevices.getUserMedia(constraints);
-  }
+    };
 
     socket.on("welcome", (data: any) => {
-      doorOpen.play()
+      doorOpen.play();
       setEnterChat(data);
-      setShowToast2(true)
-      
+      setShowToast2(true);
+
       // console.log(data);
     });
     socket.on("inroom", (data: any) => {
@@ -74,11 +80,11 @@ function ChatClient() {
       // console.log(data)
     });
     socket.on("bye", (data: any) => {
-      doorClose.play()
+      doorClose.play();
 
       setNumber(data);
       setLeftChat(data);
-      setShowToast1(true)
+      setShowToast1(true);
     });
     socket.on("otherTyping", (data: any) => {
       setIsTyping(data.id);
@@ -86,127 +92,139 @@ function ChatClient() {
       console.log(isTyping + "is typing");
     });
     socket.on("msg", (data: any) => {
-          shyaboi.play()
-          setChatArr(data)
-          scrollToBottom()
-        });
-        socket.on("message", (data: any) => {
-          setChatArr(data)
-          scrollToBottom()
-        });
-      }, []);
-      
-      const thing = () => {
-        const id = socket.id;
-        socket.emit("typing", { id });
-        // console.log(isTyping+'is typing')
-      };
-      const chatting = (e:any) => {
-        e.preventDefault();
-        console.log(chatArr)
-    const id = socket.id
-    socket.emit('message', {id:id , msg:input})
-    setInput('')
-    scrollToBottom()
-    console.log('cahtttt')
-  }
+      shyaboi.play();
+      setChatArr(data);
+      scrollToBottom();
+    });
+    socket.on("message", (data: any) => {
+      setChatArr(data);
+      scrollToBottom();
+    });
+  }, []);
+
+  const thing = () => {
+    const id = socket.id;
+    socket.emit("typing", { id });
+    // console.log(isTyping+'is typing')
+  };
+  const chatting = (e: any) => {
+    e.preventDefault();
+    console.log(chatArr);
+    const id = socket.id;
+    socket.emit("message", { id: id, msg: input });
+    setInput("");
+    scrollToBottom();
+    console.log("cahtttt");
+  };
 
   const usersInRoom = Object.values(inRoom).map((user) => (
     <IonItem>
-    
       <IonLabel key={user}>User ID: {user}</IonLabel>
     </IonItem>
   ));
-  let chatLogArr = Object.values(chatArr) as any
-  const chat = chatLogArr.map((msg:any ) => (
+  let chatLogArr = Object.values(chatArr) as any;
+  const chat = chatLogArr.map((msg: any) => (
     <IonItem>
-
-      <IonLabel >
+      <IonLabel>
         <h4>ID: {msg.id}</h4>
-<br></br>
-      <p >Message: {msg.msg}</p>
+        <br></br>
+        <p>Message: {msg.msg}</p>
       </IonLabel>
-      </IonItem>
-
+    </IonItem>
   ));
   return (
     <IonApp>
-  <IonHeader >
-  <IonItem>
-      <IonAvatar slot="start">
-        <img src="https://i.ibb.co/k6yNxLh/ha.png" />
-      </IonAvatar>
-      <IonLabel>Dingus Chat</IonLabel>
-      
-        <p > Currently {inRoom.length} in the chat.</p>
-    </IonItem>
+      <IonHeader>
+        <IonItem>
+          <IonAvatar slot="start">
+            <img src="https://i.ibb.co/k6yNxLh/ha.png" />
+          </IonAvatar>
+          <IonLabel>Dingus Chat</IonLabel>
+
+          <p> Currently {inRoom.length} in the chat.</p>
+        </IonItem>
       </IonHeader>
-        {/* <IonList>{usersInRoom}</IonList> */}
-        
-        <IonToast
-          isOpen={showToast1}
-          onDidDismiss={() => setShowToast1(false)}
-          message={leftChat+' Left the chat :('}
-          duration={1500}
-          position='middle'
-          />
-        <IonToast
-          isOpen={showToast2}
-          onDidDismiss={() => setShowToast2(false)}
-          message={enterChat+'just joined chat!'}
-          duration={1500}
-          position='middle'
-          />
-         
+      {/* <IonList>{usersInRoom}</IonList> */}
+
+      <IonToast
+        isOpen={showToast1}
+        onDidDismiss={() => setShowToast1(false)}
+        message={leftChat + " Left the chat :("}
+        duration={1500}
+        position="middle"
+      />
+      <IonToast
+        isOpen={showToast2}
+        onDidDismiss={() => setShowToast2(false)}
+        message={enterChat + "just joined chat!"}
+        duration={1500}
+        position="middle"
+      />
+
       <IonContent>
-      <IonList id='chatList'>
-        {chat}
-        <br></br>
+        <IonList id="chatList">
+          {chat}
+          <br></br>
 
-        <br></br>
-        <br></br>
-        <div ref={bottomRef} className="list-bottom">Bottom of the chat</div>
-
-      </IonList>
-      <IonFab vertical="top" horizontal="end" slot="fixed">
+          <br></br>
+          <br></br>
+          <div ref={bottomRef} className="list-bottom">
+            Bottom of the chat
+          </div>
+        </IonList>
+        <IonFab vertical="top" horizontal="end" slot="fixed">
           <IonFabButton color="secondary">
-            <IonIcon icon={peopleCircleOutline} onClick={() => setShowModal(true)} />{inRoom.length}
+            <IonIcon
+              icon={peopleCircleOutline}
+              onClick={() => setShowModal(true)}
+            />
+            {inRoom.length}
           </IonFabButton>
           <a href="http://dinguscrew.com:8080">
-          <IonFabButton color="secondary">
-            <IonIcon icon={imagesOutline} />
-          </IonFabButton>
+            <IonFabButton color="secondary">
+              <IonIcon icon={imagesOutline} />
+            </IonFabButton>
           </a>
           <a href="http://dinguscrew.com:5000/forum">
-          <IonFabButton color="secondary">
-            <IonIcon icon={megaphoneOutline} />
-          </IonFabButton>
+            <IonFabButton color="secondary">
+              <IonIcon icon={megaphoneOutline} />
+            </IonFabButton>
           </a>
         </IonFab>
-      <IonModal isOpen={showModal} cssClass='my-custom-class'>
-        <IonList>
-          {usersInRoom}
-        </IonList>
-        <IonButton color="secondary" onClick={() => setShowModal(false)}>Close</IonButton>
-      </IonModal>
+        <IonModal isOpen={showModal} cssClass="my-custom-class">
+          <IonList>{usersInRoom}</IonList>
+          <IonButton color="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </IonButton>
+        </IonModal>
       </IonContent>
       <Voice />
       <IonItem>
         <IonLabel position="stacked">Enter chat message</IonLabel>
-        <form onSubmit={chatting} >
-        <IonInput
-        color="secondary"
-        ion-input='submit'
-        size={350}
-          value={input}
-          placeholder="Shout out your people!"
-          onInput={thing}
-          onIonChange={(e) => setInput((e.target as HTMLTextAreaElement).value)}
-        />
+        <form onSubmit={chatting}>
+          <IonInput
+            color="secondary"
+            ion-input="submit"
+            size={350}
+            value={input}
+            placeholder="Shout out your people!"
+            onInput={thing}
+            onIonChange={(e) =>
+              setInput((e.target as HTMLTextAreaElement).value)
+            }
+          />
         </form>
       </IonItem>
-        <IonButton slot="end" expand="full" shape='round' onSubmit={chatting} onClick={chatting} color="secondary">Send</IonButton>
-      
+      <IonButton
+        slot="end"
+        expand="full"
+        shape="round"
+        onSubmit={chatting}
+        onClick={chatting}
+        color="secondary"
+      >
+        Send
+      </IonButton>
     </IonApp>
   );
 }
--- src/components/Voice/Voice.tsx
+++ src/components/Voice/Voice.tsx
@@ -1,93 +1,93 @@
-import React, { useState, useEffect, useRef } from "react";
-import socketIOClient from "socket.io-client";
-const ENDPOINT = "https://simpvserv.herokuapp.com/";
-const socket = socketIOClient(ENDPOINT);
+// import React, { useState, useEffect, useRef } from "react";
+// import socketIOClient from "socket.io-client";
+// const ENDPOINT = "https://simpvserv.herokuapp.com/";
+// const socket = socketIOClient(ENDPOINT);
 
 
-const constraints  = {
-  audio: true,
-  video: false
-};
+// const constraints  = {
+//   audio: true,
+//   video: false
+// };
 
 
-function Voice() {
-  // const [muted, setMuted] = useState(false);
+// function Voice() {
+//   // const [muted, setMuted] = useState(false);
 
-  // const audio = useRef();
-  // const video = useRef();
-  // const otherVideo = useRef();
+//   // const audio = useRef();
+//   // const video = useRef();
+//   // const otherVideo = useRef();
 
-  useEffect(() => {
+//   useEffect(() => {
    
 
-  })
+//   })
 
 
-  navigator.mediaDevices.getUserMedia(constraints)
-.then((stream)=> {
+//   navigator.mediaDevices.getUserMedia(constraints)
+// .then((stream)=> {
   
-  var mediaRecorder:any = new MediaRecorder(stream, {
-    mimeType: 'audio/webm'});
-  mediaRecorder.onstart = function(e:any) {
-    this.chunks = [];
-  };
-  mediaRecorder.ondataavailable = function(e:any) {
-    this.chunks.push(e.data);
-    var blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
-    socket.emit('audio', blob);
+//   var mediaRecorder:any = new MediaRecorder(stream, {
+//     mimeType: 'audio/webm'});
+//   mediaRecorder.onstart = function(e:any) {
+//     this.chunks = [];
+//   };
+//   mediaRecorder.ondataavailable = function(e:any) {
+//     this.chunks.push(e.data);
+//     var blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
+//     socket.emit('audio', blob);
 
-    // mediaRecorder.start();
-  };
-  //   mediaRecorder.onstop = function(e) {
-    // };
+//     // mediaRecorder.start();
+//   };
+//   //   mediaRecorder.onstop = function(e) {
+//     // };
     
-    mediaRecorder.start();
+//     mediaRecorder.start();
     
-    // Start recording
+//     // Start recording
     
-    // Stop recording after 5 seconds and broadcast it server
-   var dinus =  setInterval(() => {
-  //     // setTimeout(function() {
-        mediaRecorder.stop()
-        mediaRecorder.start();
-      }, 800);
-    // }, 100);
-    // console.log(stream)
-    // if (video.current) {
-    //   var userVideoStream = stream
-    //   video.current.srcObject = stream
-    // }
-      socket.on('got', function(arrayBuffer:any) {
+//     // Stop recording after 5 seconds and broadcast it server
+//    var dinus =  setInterval(() => {
+//   //     // setTimeout(function() {
+//         mediaRecorder.stop()
+//         mediaRecorder.start();
+//       }, 800);
+//     // }, 100);
+//     // console.log(stream)
+//     // if (video.current) {
+//     //   var userVideoStream = stream
+//     //   video.current.srcObject = stream
+//     // }
+//       socket.on('got', function(arrayBuffer:any) {
         
-        var blob = new Blob([arrayBuffer], { 'type' : 'audio/ogg; codecs=opus' });
-        var audio = document.createElement('audio');
-        audio.src = window.URL.createObjectURL(blob);
-        audio.play();
-        // console.log(audio)
-      });
-      // }
+//         var blob = new Blob([arrayBuffer], { 'type' : 'audio/ogg; codecs=opus' });
+//         var audio = document.createElement('audio');
+//         audio.src = window.URL.createObjectURL(blob);
+//         audio.play();
+//         // console.log(audio)
+//       });
+//       // }
      
 
-    })
+//     })
 
 
-    // function enableMute() { 
-    //     setMuted(true)
-    // } 
+//     // function enableMute() { 
+//     //     setMuted(true)
+//     // } 
     
 
-    // navigator.mediaDevices.getUserMedia(constraints).then(stream=>{
+//     // navigator.mediaDevices.getUserMedia(constraints).then(stream=>{
       
-      // });
+//       // });
       
-      return (
-        <div className="App">
-      <header className="App-header">
-      <div id="container">
-</div>
-      </header>
-    </div>
-  );
-}
+//       return (
+//         <div className="App">
+//       <header className="App-header">
+//       <div id="container">
+// </div>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default Voice;
+// export default Voice;
